fb01a914fa7859739ef8cce3417d1f5bdf70b8f1
David Benages <79161120+Divangus@users.noreply.github.com>
2022-10-27 01:53:51 +0200

    avances

--- task1/lib/main.dart
+++ task1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,29 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.amber),
-      //home: const ProductInfoScreen(),
-    );
+    return const MaterialApp(home: Disney());
+  }
+}
+
+class Disney extends StatelessWidget {
+  const Disney({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: todo
+    // TODO: implement build
+    return Scaffold(
+        body: Center(
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: const [Icon(Icons.file_upload_outlined)])
+          ],
+        ),
+      ),
+    ));
   }
 }
