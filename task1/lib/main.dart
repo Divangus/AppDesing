ce6250f25e93c789dc7eb89c70a6ea3309f78b00
David Benages <79161120+Divangus@users.noreply.github.com>
2022-11-04 21:20:40 +0100

    cositaaasss

--- task1/lib/main.dart
+++ task1/lib/main.dart
@@ -28,8 +28,8 @@ class Disney extends StatelessWidget {
       children: [
         Image.asset(
           'assets/thor_banner.jpeg',
-          width: 600,
-          height: 290,
+          width: 700,
+          height: 282,
         ),
         Container(
             decoration: const BoxDecoration(
@@ -40,7 +40,7 @@ class Disney extends StatelessWidget {
             ],
                     stops: [
               0.1,
-              0.3
+              0.29
             ],
                     begin: FractionalOffset.topCenter,
                     end: FractionalOffset.bottomCenter))),
@@ -54,6 +54,7 @@ class Disney extends StatelessWidget {
                   size: 30,
                   color: Colors.white,
                 ),
+                const SizedBox(width: 20),
                 Container(
                     width: 25,
                     height: 25,
@@ -97,8 +98,218 @@ class Disney extends StatelessWidget {
                           color: Colors.white,
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
-                        )))
-              ])
+                        ))),
+                const SizedBox(
+                  width: 5,
+                ),
+                Container(
+                  width: 165,
+                  height: 30,
+                  padding: const EdgeInsets.all(3),
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 49, 52, 61),
+                      borderRadius: BorderRadius.all(Radius.circular(5))),
+                  child: Row(children: const [
+                    Text('IMAX',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        )),
+                    Text(' ENHANCED',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 20,
+                        ))
+                  ]),
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Container(
+                  width: 120,
+                  height: 30,
+                  padding: const EdgeInsets.all(3),
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 49, 52, 61),
+                      borderRadius: BorderRadius.all(Radius.circular(5))),
+                  child: Row(children: [
+                    Image.asset(
+                      'assets/dolby.png',
+                      width: 28,
+                      height: 18,
+                    ),
+                    const Text('Dolby',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        )),
+                    const Text(' Vision',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 15,
+                        ))
+                  ]),
+                )
+              ]),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    '2017',
+                    style: TextStyle(color: Color.fromARGB(255, 202, 202, 202)),
+                  ),
+                  Text(
+                    ' · ',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 202, 202, 202),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
+                    ),
+                  ),
+                  Text(
+                    '2 h 12 min',
+                    style: TextStyle(color: Color.fromARGB(255, 202, 202, 202)),
+                  ),
+                  Text(
+                    ' · ',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 202, 202, 202),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
+                    ),
+                  ),
+                  Text(
+                    'Ciencia ficción, Comedia, Fantásticas,',
+                    style: TextStyle(color: Color.fromARGB(255, 202, 202, 202)),
+                  ),
+                ],
+              ),
+              const Text(
+                'Superhéroe, Acción y aventura',
+                style: TextStyle(color: Color.fromARGB(255, 202, 202, 202)),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                  width: 450,
+                  height: 50,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.play_arrow_rounded,
+                        size: 35,
+                        color: Colors.black,
+                      ),
+                      Text(
+                        'VER',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 3,
+                            fontSize: 16),
+                      )
+                    ],
+                  )),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: const [
+                      Icon(Icons.add,
+                          size: 35, color: Color.fromARGB(255, 202, 202, 202)),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Mi Lista',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 202, 202, 202)),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    width: 50,
+                  ),
+                  Column(
+                    children: const [
+                      Icon(Icons.group_rounded,
+                          size: 35, color: Color.fromARGB(255, 202, 202, 202)),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'GroupWatch',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 202, 202, 202)),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    width: 50,
+                  ),
+                  Column(
+                    children: const [
+                      Icon(Icons.video_collection_rounded,
+                          size: 35, color: Color.fromARGB(255, 202, 202, 202)),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Tráiler',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 202, 202, 202)),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    width: 50,
+                  ),
+                  Column(
+                    children: const [
+                      Icon(Icons.download_rounded,
+                          size: 35, color: Color.fromARGB(255, 202, 202, 202)),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Descargar',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 202, 202, 202)),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                'Thor debe apresurarse para evitar que',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 202, 202, 202), fontSize: 25),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const Text(
+                'Asgard no caiga en las manos de Hela.',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 202, 202, 202), fontSize: 25),
+              )
             ],
           ),
         ),
